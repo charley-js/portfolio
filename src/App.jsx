06c96527351f91694edb4c42fc05a4132631c7b8
charley-js <charleycrowson1997@gmail.com>
2024-07-30 19:08:39 +0100

    This commit creates the DateTime component and renders it on every route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/layout/Sidebar";
+import DateTime from "./components/shared/DateTime";
 
 const App = () => {
   return (
     <Router>
       <Sidebar />
+      <DateTime />
       <Routes>
         <Route path="/home" />
         <Route path="/projects" />
