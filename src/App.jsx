05600c90ba2ce55d79f7e37a01cc5ba5bb55936c
charley-js <charleycrowson1997@gmail.com>
2024-07-30 17:31:55 +0100

    Initialized React project and dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./assets/styles/index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">My Portfolio</h1>
+    </>
+  );
+}
+
+export default App;
